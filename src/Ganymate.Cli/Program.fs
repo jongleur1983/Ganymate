1534ed2a083b5dbdd6e30201769474d32ad72fd8
TeaDrivenDev <TeaDrivenDev@gmail.com>
2020-09-04 23:06:42 +0200

    Move CLI argument handling to separate module

--- src/Ganymate.Cli/Program.fs
+++ src/Ganymate.Cli/Program.fs
@@ -7,18 +7,8 @@ open Argu
 
 open Ganymate
 
-module Program =
-    let printVersion () =
-        let productName, version =
-            let assembly = System.Reflection.Assembly.GetExecutingAssembly()
-
-            let fileVersionInfo =
-                System.Diagnostics.FileVersionInfo.GetVersionInfo assembly.Location
-
-            fileVersionInfo.ProductName, assembly.GetName().Version
-
-        printfn "%s v%i.%i.%i" productName version.Major version.Minor version.Build
-
+[<AutoOpen>]
+module Prelude =
     let traceColored color (s : string) =
         let curColor = Console.ForegroundColor
         if curColor <> color then Console.ForegroundColor <- color
@@ -30,6 +20,23 @@ module Program =
 
         textWriter.WriteLine s
         if curColor <> color then Console.ForegroundColor <- curColor
+
+[<AutoOpen>]
+module CliHandling =
+    type CommandLineParameters =
+        {
+            IsValid: bool
+            Directory: string
+            IsVerbose: bool
+            ShowHelp: bool
+        } with
+            static member Empty =
+                {
+                    IsValid = false
+                    Directory = ""
+                    IsVerbose = false
+                    ShowHelp = false
+                }
 
     type GanymateExiter() =
         interface IExiter with
@@ -51,65 +58,61 @@ module Program =
                     | Directory _ -> "The directory containing the Git repository"
                     | Verbose -> "Show additional output for debugging and similar"
 
-    type ExitCode =
-        | Ok = 0
-        | NoDirectory = 1
-        | NoRepository = 2
-
-    let getRepositoryInfo directory =
-        [
-            sprintf "Showing commits in %s" directory
-            sprintf "HEAD is %s" (GitRepository.getHead directory)
-        ]
-    
-    type CommandLineParameters =
-        {
-            IsValid: bool
-            Directory: string
-            IsVerbose: bool
-            ShowHelp: bool
-        } with
-            static member Empty =
-                {
-                    IsValid = false
-                    Directory = ""
-                    IsVerbose = false
-                    ShowHelp = false
-                }
-        
-        
     let rec collectCommandLineParameters remainingArguments parameters =
         match remainingArguments with
         | head :: tail ->
-            let parameters = 
+            let parameters =
                 match head with
                 | Directory directory -> { parameters with Directory = directory }
                 | Verbose -> { parameters with IsVerbose = true }
-            
+
             collectCommandLineParameters tail parameters
         | [] ->
             match parameters.Directory with
             | "" | "." ->
                 { parameters with Directory = Directory.GetCurrentDirectory() }
             | _ -> parameters
-        
+
     let processCommandLineArguments (parser: ArgumentParser<CliArgument>) argv =
         let results = parser.ParseCommandLine argv
         let cliArguments = results.GetAllResults()
-        
+
         if results.IsUsageRequested
         then { CommandLineParameters.Empty with ShowHelp = true }
         else
             CommandLineParameters.Empty
             |> collectCommandLineParameters cliArguments
-        
+
+module Program =
+    let printVersion () =
+        let productName, version =
+            let assembly = System.Reflection.Assembly.GetExecutingAssembly()
+
+            let fileVersionInfo =
+                System.Diagnostics.FileVersionInfo.GetVersionInfo assembly.Location
+
+            fileVersionInfo.ProductName, assembly.GetName().Version
+
+        printfn "%s v%i.%i.%i" productName version.Major version.Minor version.Build
+
+    type ExitCode =
+        | Ok = 0
+        | NoDirectory = 1
+        | NoRepository = 2
+
     let (|Repository|Directory|NoDirectory|) directory =
         if Directory.Exists directory
         then
             if GitRepository.isGitRepository directory
             then Repository
             else Directory
-        else NoDirectory        
+        else NoDirectory
+
+    let getRepositoryInfo directory =
+        [
+            sprintf "Showing commits in %s" directory
+            sprintf "HEAD is %s" (GitRepository.getHead directory)
+        ]
 
     [<EntryPoint>]
     let main argv =
@@ -119,9 +122,9 @@ module Program =
                 helpTextMessage = "Help requested",
                 errorHandler = GanymateExiter())
         let parameters = processCommandLineArguments parser argv
-        
+
         if parameters.IsVerbose then printfn "Got parse results %A" ()
-        
+
         let exitCode, output =
             if parameters.ShowHelp
             then ExitCode.Ok, [ parser.PrintUsage() ]
@@ -135,5 +138,5 @@ module Program =
                     ExitCode.NoDirectory, [ sprintf "Directory %s is not a directory" parameters.Directory ]
 
         output |> List.iter (printfn "%s")
-        
+
         int exitCode
