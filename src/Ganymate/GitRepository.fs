83d736f02b887cfec0453930e469c3b7ce62a3ef
TeaDrivenDev <TeaDrivenDev@gmail.com>
2020-09-05 19:55:05 +0200

    Stash commit

--- src/Ganymate/GitRepository.fs
+++ src/Ganymate/GitRepository.fs
@@ -30,6 +30,9 @@ module GitRepository =
             Diff: string
         }
 
+    //type Repository(repository: LibGit2Sharp.Repository) =
+    //    class end
+
     let isGitRepository directory =
         LibGit2Sharp.Repository.IsValid directory
 
@@ -37,7 +40,34 @@ module GitRepository =
         use repository = new LibGit2Sharp.Repository(directory)
 
         repository.Head.FriendlyName
-        
+
+    let createDiffOfTrees (repository: Repository) (compareOptions: CompareOptions) (oldTree: Tree) (newTree: Tree) =
+        repository.Diff.Compare<TreeChanges>(oldTree, newTree, compareOptions)
+        |> Seq.filter (fun (change: TreeEntryChanges) -> change.Status <> ChangeKind.Unmodified)
+        |> Seq.map (fun change -> change.Path, change.Status)
+
+    let getLastCommitChanges directory =
+        use repository = new LibGit2Sharp.Repository(directory)
+
+        let last :: previous :: _ =
+            repository.Head.Commits
+            |> Seq.toList
+
+        let x = 1
+
+        createDiffOfTrees repository (CompareOptions()) previous.Tree last.Tree
+
+    let getLastCommitDiff directory =
+        use repository = new LibGit2Sharp.Repository(directory)
+
+        let last :: previous :: _ =
+            repository.Head.Commits
+            |> Seq.toList
+
+        let patch = repository.Diff.Compare<Patch>(previous.Tree, last.Tree)
+
+        patch.Content
+
     let createCommitter (signature: Signature) =
         {
             Name = signature.Name
@@ -56,9 +86,30 @@ module GitRepository =
                 Committer = createCommitter commit.Committer
                 MessageShort = commit.MessageShort
                 Files = []
+                    //commit.Parents
+                    //|> Seq.tryHead
+                    //|> Option.map (fun parent ->
+                    //    )
                 Diff = ""
             })
         |> Seq.toList
+
+//    let createDiffOfMergedTrees (compareOptions: CompareOptions) (tree: Tree) (parents: Commit seq) (repository: Repository) =
+//        repository.Diff.Compare(oldTree, newTree, compareOptions)
+//        |> Seq.filter (fun (change: TreeEntryChanges) -> change.Status = ChangeKind.Unmodified)
+
+//private IEnumerable<TreeEntryChanges> GetDiffOfMergedTrees(Repository gitRepo, IEnumerable<LibGit2Sharp.Commit> parents, Tree tree, CompareOptions compareOptions)
+//{
+//        var firstParent = parents.ElementAt(0);
+//        var secondParent = parents.ElementAt(1);
+
+//        var firstChanges = GetDiffOfTrees(gitRepo, firstParent.Tree, tree, compareOptions);
+//        var secondChanges = GetDiffOfTrees(gitRepo, secondParent.Tree, tree, compareOptions);
+
+//        var changes = firstChanges.Where(c1 => secondChanges.Any(c2 => c2.Oid == c1.Oid));
+
+//        return changes;
+//}
 
     let getCommits directory =
         use repository = new LibGit2Sharp.Repository(directory)
