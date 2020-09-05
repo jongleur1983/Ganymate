dc08695744b47d8c020891187a349012b1b0c93c
Peter Amrehn <peter.wendorff@gmx.de>
2020-09-05 19:54:24 +0200

    Create Window

--- src/Ganymate.Visualization/Library.fs
+++ src/Ganymate.Visualization/Library.fs
@@ -1,6 +1,31 @@
 ﻿namespace Ganymate.Visualization
 
+open Veldrid
+open Veldrid.StartupUtilities
+
 module Program =
+    let createWindow =
+        let windowCreateInfo =
+            WindowCreateInfo(
+                X = 400,
+                Y = 400,
+                WindowHeight = 640,
+                WindowWidth = 480,
+                WindowTitle = "Ganymate.Visualization"            
+            )
+        let window = VeldridStartup.CreateWindow windowCreateInfo
+        window.add_Closed (fun () -> exit 0  |>ignore )
+        window.Resizable <- true
+        window.BorderVisible <-true
+        window
+        
     [<EntryPoint>]
     let main argv =
+        let window = createWindow
+        let gdo = GraphicsDeviceOptions()
+        let gd = VeldridStartup.CreateGraphicsDevice window
+        
+        while window.Exists do
+            window.PumpEvents()
+            
         0
