e77237cc9a2f4c09e91864b8a79f9bd25b815724
Peter Amrehn <peter.wendorff@gmx.de>
2020-09-14 21:20:25 +0200

    refactor: recursive drawing loop and slighly working click event

--- src/Ganymate.Visualization/Library.fs
+++ src/Ganymate.Visualization/Library.fs
@@ -21,7 +21,74 @@ module Program =
         window.Resizable <- true
         window.BorderVisible <-true
         window
-        
+
+    let drawFrame (window:Sdl2.Sdl2Window) sw (gui:ImGuiRenderer) isClicked (cl:CommandList) (gd:GraphicsDevice) elapsedTime =
+        let events = window.PumpEvents()
+        let frameRate = 1000000.0f / float32 elapsedTime
+        gui.Update(float32 elapsedTime, events)
+        ImGui.SetNextWindowSize(Vector2(float32 (window.Width), float32 window.Height * 0.25f))
+        ImGui.SetNextWindowPos(Vector2(float32 0, float32 window.Height * 0.75f), ImGuiCond.Always)
+
+        ImGui.Begin(
+            "main",
+            ImGuiWindowFlags.NoMove
+            ||| ImGuiWindowFlags.NoCollapse
+            ||| ImGuiWindowFlags.NoTitleBar
+            ||| ImGuiWindowFlags.NoResize)
+        |> ignore
+
+        let text = sprintf "FPS %6.2f" frameRate
+
+        ImGui.Text(text)
+
+        ImGui.SetNextWindowPos(Vector2(10.f, 20.f))
+        ImGui.Separator()
+        ImGui.Indent 25.f
+
+        isClicked
+        |> Option.map (fun _ -> "IsClicked")
+        |> Option.defaultValue "--"
+        |> ImGui.Text
+
+        ImGui.Button("test")
+        let isJustClicked = ImGui.IsItemClicked ImGuiMouseButton.Left
+        ImGui.End()
+
+        cl.Begin()
+        cl.SetFramebuffer(gd.MainSwapchain.Framebuffer)
+
+        if ImGui.GetIO().WantCaptureMouse
+        then cl.ClearColorTarget(uint32 0, RgbaFloat.Black)
+        else cl.ClearColorTarget(uint32 0, RgbaFloat.CornflowerBlue)
+
+        gui.Render (gd, cl)
+        cl.End()
+
+        gd.SubmitCommands cl
+        gd.SwapBuffers()
+        isJustClicked
+
+
+    let rec drawFrameOrExit (sw:Stopwatch) isClicked (window:Sdl2.Sdl2Window)  gui cl gd lastFrame =
+        if not window.Exists
+        then 1
+        else
+            // actual drawing code
+            let currentFrame = sw.ElapsedTicks
+            let elapsedTime = currentFrame - lastFrame
+
+            let isJustClicked = drawFrame window sw gui isClicked cl gd elapsedTime
+            let isClickedNew =
+                match isClicked with
+                | Some 0 -> None
+                | Some x -> Some (x - 1)
+                | None ->
+                    if isJustClicked
+                    then Some 20_000
+                    else None
+
+            drawFrameOrExit sw isClickedNew window gui cl gd currentFrame
+
     [<EntryPoint>]
     let main argv =
         let window = createWindow
@@ -39,57 +106,8 @@ module Program =
         window.add_Resized (fun () ->
             gd.ResizeMainWindow(uint32 window.Width, uint32 window.Height))
         let mutable lastFrame = sw.ElapsedTicks
-        let mutable isClicked = false
 
         let cl = gd.ResourceFactory.CreateCommandList()
-        while window.Exists do
-            let events = window.PumpEvents()
-            
-            let currentFrame = sw.ElapsedTicks
-            let elapsedTime = currentFrame - lastFrame
-            let frameRate = 1000000.0f / float32 elapsedTime
-            
-            gui.Update(float32 elapsedTime, events)
-            
-            ImGui.SetNextWindowSize(Vector2(float32 (window.Width), float32 window.Height * 0.25f))
-            ImGui.SetNextWindowPos(Vector2(float32 0, float32 window.Height * 0.75f), ImGuiCond.Always)
-
-            ImGui.Begin(
-                "main",
-                ImGuiWindowFlags.NoMove
-                ||| ImGuiWindowFlags.NoCollapse
-                ||| ImGuiWindowFlags.NoTitleBar
-                ||| ImGuiWindowFlags.NoResize)
-            |> ignore
-
-            let text = sprintf "FPS %6.2f" frameRate
-                
-            ImGui.Text(text)
-
-            ImGui.SetNextWindowPos(Vector2(10.f, 20.f))
-            ImGui.Separator()
-            ImGui.Indent 25.f
-
-            ImGui.Button("test")
-            let isClickedNew = ImGui.IsItemClicked ImGuiMouseButton.Left
-            isClicked <- isClickedNew || isClicked
-
-            ImGui.Text(isClicked.ToString())
-
-            ImGui.End()
-            
-            cl.Begin()
-            cl.SetFramebuffer(gd.MainSwapchain.Framebuffer)
-            
-            if ImGui.GetIO().WantCaptureMouse
-            then cl.ClearColorTarget(uint32 0, RgbaFloat.Black)
-            else cl.ClearColorTarget(uint32 0, RgbaFloat.CornflowerBlue)
-            
-            gui.Render (gd, cl)
-            cl.End()
-            
-            gd.SubmitCommands cl
-            gd.SwapBuffers()
-            
-            lastFrame <- currentFrame
-        0
+        let lastFrame = sw.ElapsedTicks
+        let isClicked = Some 10_000
+        drawFrameOrExit sw isClicked window gui cl gd lastFrame
